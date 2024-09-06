63580cc6330837832cf72d77a6aca32c1172fe36
aneebamoezz <anibakaleem@gmail.com>
2024-09-07 04:00:30 +0500

    update files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,15 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
+    fontFamily:{
+      inter:["Inter", "serif"],
+    },
     extend: {
       colors:{
         "purple-shade":"rgb(79, 70, 229)",
-      }
+        "gray-color":"rgb(55, 65, 81)",
+        "black-shade":"rgb(17, 24, 39)",
+      },
     },
   },
   plugins: [],
