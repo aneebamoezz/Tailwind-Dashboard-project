4bc4d7f90bb6e342af52f03d07664407cbe9d43f
aneebamoezz <anibakaleem@gmail.com>
2024-09-10 18:34:26 +0500

    Update files

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
         "purple-shade":"rgb(79, 70, 229)",
         "gray-color":"rgb(55, 65, 81)",
         "black-shade":"rgb(17, 24, 39)",
+        "light-gray":"#e5e7eb",
+        "lightgray-400":"#94a3b8",
       },
     },
   },
