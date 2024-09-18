81a22da9f5a3bdc16ca0a3256f097148255d9387
aneebamoezz <anibakaleem@gmail.com>
2024-09-18 22:11:11 +0500

    Update files

---
+++ app.js
@@ -0,0 +1,7 @@
+const moon = document.querySelector("#moon");
+const body = document.querySelector("body");
+
+
+moon.addEventListener("click", () => {
+    body.classList.toggle("dark")
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./*.html"],
   theme: {
-    fontFamily:{
-      inter:["Inter", "serif"],
+    fontFamily: {
+      inter: ["Inter", "serif"],
     },
     extend: {
-      colors:{
-        "purple-shade":"rgb(79, 70, 229)",
-        "gray-color":"rgb(55, 65, 81)",
-        "black-shade":"rgb(17, 24, 39)",
-        "light-gray":"#e5e7eb",
-        "lightgray-400":"#94a3b8",
-        "body-gray":"#f3f4f6",
-        "blue-color":"rgb(59, 130, 246)",
-        "black-shade":"rgb(9, 9, 11)",
-        "gray-text":"rgb(113, 113, 122)"
+      boxShadow: {
+        'shadowmd': '0 35px -60px -15px rgba(55, 65, 81, 0.3)',
+      },
+      colors: {
+        "purple-shade": "rgb(79, 70, 229)",
+        "gray-color": "rgb(55, 65, 81)",
+        "black-shade": "rgb(17, 24, 39)",
+        "light-gray": "#e5e7eb",
+        "lightgray-400": "#94a3b8",
+        "body-gray": "#f3f4f6",
+        "blue-color": "rgb(59, 130, 246)",
+        "black-shade": "rgb(9, 9, 11)",
+        "gray-text": "rgb(113, 113, 122)",
+        "darkmode-100": "#1a1625",
+        "darkmode-300": "#2f2b3a",
+        "primary-500": "#a688fa",
+        "dark-600":"#dbdbdb",
+        "dark-200":"#3f3f3f",
+        "inputdark-200":"#2f2b3a",
       },
     },
   },
   plugins: [],
-}
-
+};
